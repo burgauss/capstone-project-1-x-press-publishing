24d6cebc35d98bca496421fe0eddc5a8a386e93d
Juan Burgos <burgos.135@gmail.com>
2024-10-11 08:52:10 -0600

    implemented the get for issuesRouter but with errors

--- api/issues.js
+++ api/issues.js
@@ -5,10 +5,14 @@ const db = new sqlite.Database(process.env.TEST_DATABASE || './database.sqlite')
 const issuesRouter = express.Router({mergeParams: true});
 
 issuesRouter.get('/', (req, res, next)=>{
-    console.log(`issues router get`);
-    // db.all("SELECT * FROM Issue WHERE series_id = $seriesId", {
-    //     $seriesId: req.params.
-    // })
+    db.all("SELECT * FROM Issue WHERE series_id = $seriesId",{$seriesId: req.params.seriesId}, (err, issues) => {
+        if(err){
+            next(err);
+        } else{
+            res.status(200).send({issues: issues});
+        }
+
+    })
 });
 
 module.exports = issuesRouter;
